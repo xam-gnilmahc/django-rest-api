f72d59dc0766bc80706b1c03050c29338bcf2007
max98703 <max.c@shikhartech.com>
2025-07-21 16:32:53 +0545

    webhook

--- tutorials/models/payment_log.py
+++ tutorials/models/payment_log.py
@@ -1,11 +1,8 @@
-from django.contrib.auth.models import User
+
 from django.db import models
 
 
 class PaymentLog(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="payment_logs"
-    )
 
     payment_gateway_name = models.CharField(max_length=255)
     transaction_id = models.CharField(max_length=255, blank=True, null=True)
@@ -17,9 +14,6 @@ class PaymentLog(models.Model):
 
     response_data = models.TextField(blank=True, null=True)  # full gateway response
     request_data = models.TextField(blank=True, null=True)   # request sent to gateway
-
-    created_by = models.BigIntegerField(default=0)
-    updated_by = models.BigIntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- tutorials/urls.py
+++ tutorials/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from tutorials.views.auth_views import login_view, register_view
 from tutorials.views.user_payment_views import userPaymentGateway_list
 from tutorials.views.webhook_views import github_webhook
+from tutorials.views.stripe_webhook import stripe_webhook
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,6 +13,7 @@ urlpatterns = [
     path("api/login", login_view, name="login"),
     path("api/register", register_view, name="register"),
     path("api/github/feed", github_webhook, name="github_Webhook"),
+    path("api/stripe/feed",stripe_webhook, name="stripe_webhook"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     # Payment gateway routes
     path("api/paymentGateways", userPaymentGateway_list, name="payment_gateways"),
