b6c98cd605e8f8723c062ca5f54f2c5eea213be2
max98703 <max.c@shikhartech.com>
2025-07-23 10:46:50 +0545

    lint fix

--- tutorials/utils/response_helper.py
+++ tutorials/utils/response_helper.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-def success_response(message, data=None, status_code=status.HTTP_200_ok):
+def success_response(message, data=None, status_code=status.HTTP_200_OK):
     return Response(
         {"status": True, "message": message, "data": data}, status=status_code
     )
